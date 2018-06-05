e8869642ca316fc4f72b691510ff8942c1e301dd
CyC2018 <1029579233@qq.com>
2018-06-05 15:27:38 +0800

    备忘录模式实现

---
+++ Menento/src/PreviousCalculationToOriginator.java
@@ -0,0 +1,9 @@
+/**
+ * Memento Interface to Originator
+ * 
+ * This interface allows the originator to restore its state
+ */
+public interface PreviousCalculationToOriginator {
+	int getFirstNumber();
+	int getSecondNumber();
+}
