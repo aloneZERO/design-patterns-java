c50d7294ffd84cb4ca1858c3cfb34711f2eb2fc0
CyC2018 <1029579233@qq.com>
2018-06-05 10:13:00 +0800

    原型模式实现

---
+++ Prototype/src/Client.java
@@ -0,0 +1,7 @@
+public class Client {
+    public static void main(String[] args) {
+        Prototype prototype = new ConcretePrototype("abc");
+        Prototype clone = prototype.myClone();
+        System.out.println(clone.toString());
+    }
+}
